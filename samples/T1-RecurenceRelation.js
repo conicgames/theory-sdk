1e431866b2e983e9f163c4534fbd19eb0a7a6882
sudo-ellipsis <57691445+sudo-ellipsis@users.noreply.github.com>
2021-12-08 09:19:21 +0800

    made api documentation visible by default
    changed ../folder/ to ./folder/ so if edited in IDE documentation actually shows on hover

--- samples/T1-RecurenceRelation.js
+++ samples/T1-RecurenceRelation.js
@@ -1,8 +1,8 @@
-﻿import { ExponentialCost, FirstFreeCost, LinearCost } from "../api/Costs";
-import { Localization } from "../api/Localization";
-import { parseBigNumber, BigNumber } from "../api/BigNumber";
-import { theory } from "../api/Theory";
-import { Utils } from "../api/Utils";
+﻿import { ExponentialCost, FirstFreeCost, LinearCost } from "./api/Costs";
+import { Localization } from "./api/Localization";
+import { parseBigNumber, BigNumber } from "./api/BigNumber";
+import { theory } from "./api/Theory";
+import { Utils } from "./api/Utils";
 
 var id = "recurrence_relation";
 var name = "Recurrence Relation";
