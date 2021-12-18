17de7189a6c62ca79bfe523d1c7094b4a61c6182
conicgames <conic.games.studio@gmail.com>
2021-12-17 21:17:58 -0500

    Improve Mac version

--- sdk/TheorySDK/Serializer.cs
+++ sdk/TheorySDK/Serializer.cs
@@ -1,20 +1,29 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text.Json;
 
 namespace TheorySDK
 {
     public static class Serializer
     {
-        private static readonly string _configFilename = "./config.json";
-
         public static void Serialize(Data data)
         {
-            File.WriteAllText(_configFilename, JsonSerializer.Serialize(data));
+            File.WriteAllText(GetPath(), JsonSerializer.Serialize(data));
         }
 
         public static void Deserialize(out Data data)
         {
-            data = JsonSerializer.Deserialize<Data>(File.ReadAllText(_configFilename));
+            data = JsonSerializer.Deserialize<Data>(File.ReadAllText(GetPath()));
+        }
+
+        private static string GetPath()
+        {
+            var path = "config.json";
+
+            if (Eto.Platform.Instance.IsMac)
+                path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/TheorySDK/" + path;
+
+            return path;
         }
     }
 }
